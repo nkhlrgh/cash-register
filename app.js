2cd56426d8afb52b0ffa2c73f6a4568f6c14551c
Nikhil M R <nkhlrgh@gmail.com>
2021-02-10 14:34:24 +0530

    Feat: Add working conversation

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
-var input1 = document.querySelector("#bill");
-console.log(input1);
+var input1 = document.querySelector("#bill-input");
 var input2 = document.querySelector("#note-input");
 var display = document.querySelector(".output");
 var currencies = [2000, 500, 200, 100, 50, 20, 10, 2, 1];
 function onClickHandler() {
   var billInput = input1.value;
   var noteInput = input2.value;
-  console.log(billInput);
-  console.log(noteInput);
   var remainingAmount = noteInput - billInput;
   display.innerText = remainingAmount;
-  console.log(remainingAmount);
   currencies.map((currency) => {
     if (currency <= remainingAmount && remainingAmount > 0) {
       var notes = Math.floor(remainingAmount / currency);
@@ -19,7 +15,7 @@ function onClickHandler() {
       var node = document.createElement("LI");
       var textnode = document.createTextNode(displayOut);
       node.appendChild(textnode);
-      document.getElementById("list").appendChild(node);
+      document.getElementById("output-list").appendChild(node);
     }
   });
 }
